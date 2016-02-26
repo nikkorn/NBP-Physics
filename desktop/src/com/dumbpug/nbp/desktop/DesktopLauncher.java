5e38332219a76fd4349e8680ddd4e39cd0af66eb
nikkorn <nh163@MOWA202-A1.university.brighton.ac.uk>
2016-02-26 16:01:17 +0000

    Added NBPCluster class
    Added NBPCluster class to simulate many objects at the same time

--- desktop/src/com/dumbpug/nbp/desktop/DesktopLauncher.java
+++ desktop/src/com/dumbpug/nbp/desktop/DesktopLauncher.java
@@ -3,10 +3,11 @@ package com.dumbpug.nbp.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.dumbpug.nbp.NBP;
+import com.dumbpug.nbp.NBPCluster;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new NBP(), config);
+		new LwjglApplication(new NBPCluster(), config);
 	}
 }
