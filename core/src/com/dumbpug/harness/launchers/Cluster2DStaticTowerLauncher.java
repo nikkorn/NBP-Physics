329a2ce6401bcb69de5a5f3541d78acdcf97226e
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2018-12-12 12:51:21 +0000

    Cluster tower example improved.

--- core/src/com/dumbpug/harness/launchers/Cluster2DStaticTowerLauncher.java
+++ core/src/com/dumbpug/harness/launchers/Cluster2DStaticTowerLauncher.java
@@ -1,6 +1,7 @@
 package com.dumbpug.harness.launchers;
 
 import java.util.ArrayList;
+import java.util.Random;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -20,12 +21,14 @@ import com.dumbpug.nbp.Gravity;
 public class Cluster2DStaticTowerLauncher extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture wimg;
+    private Texture gimg;
 
     /** The physics entities. */
     private Environment environment;
-    private ArrayList<Box> cluster = new ArrayList<Box>();
+    private ArrayList<Box> staticBoxes  = new ArrayList<Box>();
+    private ArrayList<Box> dynamicBoxes = new ArrayList<Box>();
     
-    private int clusterSize = 1;
+    private int clusterSize = 500;
     
     private int towerWidth  = 100;
     private int towerHeight = 100;
@@ -33,26 +36,29 @@ public class Cluster2DStaticTowerLauncher extends ApplicationAdapter {
 
     @Override
     public void create () {
-        batch = new SpriteBatch();
-        wimg  = new Texture("white_box.png");
-
-        environment = new Environment(Dimension.TWO_DIMENSIONS, 20f, new Gravity(Axis.Y, -0.09f));
+    	Random random = new Random();
+        batch         = new SpriteBatch();
+        wimg          = new Texture("white_box.png");
+        gimg          = new Texture("green_box.png");
+        environment   = new Environment(Dimension.TWO_DIMENSIONS, 20f, new Gravity(Axis.Y, -0.09f));
         
         // Create a tower of static boxes that do not overlap.
         for(int x = 0; x < towerWidth; x++) {
         	for(int y = 0; y < towerHeight; y++) {
                 Box cBox = new Basic2DBox((x * towerBoxSize) + 80f, (y * towerBoxSize) - 300f, towerBoxSize, towerBoxSize, BoxType.STATIC);
                 environment.addBox(cBox);
-                cluster.add(cBox);
+                staticBoxes.add(cBox);
             }
         }
         
         // Create a cluster of dynamic boxes.
         for(int i = 0; i < clusterSize; i++) {
-            Box cBox = new Basic2DBox(200f, 400, 5, 5, BoxType.DYNAMIC);
-            cBox.setRestitution(0.9f); // Changing the value effects whether collision works as expected! 
+            Box cBox = new Basic2DBox((random.nextFloat()*500f) + 80f, 300f, 5, 5, BoxType.DYNAMIC);
+            cBox.applyImpulse(Axis.X, (random.nextFloat()*6f)-3f);
+            cBox.setRestitution(random.nextFloat());
+            cBox.setFriction(random.nextFloat());
             environment.addBox(cBox);
-            cluster.add(cBox);
+            dynamicBoxes.add(cBox);
         }
     }
 
@@ -66,11 +72,13 @@ public class Cluster2DStaticTowerLauncher extends ApplicationAdapter {
         // Update the physics environment.
         environment.update();
 
-        // Draw all boxes in cluster
+        // Draw all boxes.
         batch.begin();
-        for(Box box : cluster) {
-            // Draw the box.
+        for(Box box : staticBoxes) {
             batch.draw(wimg, box.getX(), box.getY(), box.getWidth(), box.getHeight());
+        }
+        for(Box box : dynamicBoxes) {
+            batch.draw(gimg, box.getX(), box.getY(), box.getWidth(), box.getHeight());
         }
         batch.end();
     }
