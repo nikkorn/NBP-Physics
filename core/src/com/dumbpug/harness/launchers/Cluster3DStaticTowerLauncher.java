daa18c4a068fcb9e436660e118da1f0d60ed4de8
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2018-12-13 12:58:51 +0000

    Starting with a fresh 3d cluster test.

--- core/src/com/dumbpug/harness/launchers/Cluster3DStaticTowerLauncher.java
+++ core/src/com/dumbpug/harness/launchers/Cluster3DStaticTowerLauncher.java
@@ -1,7 +1,6 @@
 package com.dumbpug.harness.launchers;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Random;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
@@ -9,6 +8,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
@@ -18,41 +18,27 @@ import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.dumbpug.harness.Basic3DBox;
-import com.dumbpug.nbp.Axis;
+import com.dumbpug.nbp.AABB;
 import com.dumbpug.nbp.BoxType;
-import com.dumbpug.nbp.Dimension;
-import com.dumbpug.nbp.Environment;
-import com.dumbpug.nbp.Gravity;
 
 /**
- * A simple 2D launcher with lots of individual dynamic boxes.
+ * A simple 3D launcher with lots of individual dynamic boxes.
  */
 public class Cluster3DStaticTowerLauncher extends ApplicationAdapter {
-	public com.badlogic.gdx.graphics.g3d.Environment environment;
+	public Environment environment;
 	public PerspectiveCamera cam;
 	public CameraInputController camController;
 	public ModelBatch modelBatch;
 	public Model intersectingModel;
 	public Model nonIntersectingModel;
+	
+	public ArrayList<ModelInstance> instances = new ArrayList<ModelInstance>();
 
-    /** The physics entities. */
-    private Environment world;
-    private ArrayList<Basic3DBox> boxes = new ArrayList<Basic3DBox>();
-    private HashMap<Basic3DBox, ModelInstance> boxToModelInstanceMap = new HashMap<Basic3DBox, ModelInstance>();
-    
-    private int clusterSize = 50;
-    
-    private int towerWidth  = 10;
-    private int towerHeight = 10;
-    private int towerBoxSize = 5;
-
-    @Override
-    public void create () {
-    	Random random = new Random();
-        
-        world = new Environment(Dimension.THREE_DIMENSIONS, 20f, new Gravity(Axis.Y, -0.09f));
-        
-        environment = new com.badlogic.gdx.graphics.g3d.Environment();
+	float boxSize = 5f;
+	
+	@Override
+	public void create () {
+		environment = new Environment();
 		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
 		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
 		
@@ -65,72 +51,64 @@ public class Cluster3DStaticTowerLauncher extends ApplicationAdapter {
 		cam.far = 300f;
 		cam.update();
 		
-		ModelBuilder modelBuilder = new ModelBuilder();
+        ModelBuilder modelBuilder = new ModelBuilder();
         intersectingModel = modelBuilder.createBox(5f, 5f, 5f, 
-            new Material(ColorAttribute.createDiffuse(Color.BLUE)),
+            new Material(ColorAttribute.createDiffuse(Color.RED)),
             Usage.Position | Usage.Normal);
         nonIntersectingModel = modelBuilder.createBox(5f, 5f, 5f, 
                 new Material(ColorAttribute.createDiffuse(Color.GREEN)),
                 Usage.Position | Usage.Normal);
-		
         
-        // Create a tower of static boxes that do not overlap.
-        for(int x = 0; x < towerWidth; x++) {
-        	for(int y = 0; y < towerWidth; y++) {
-        		for(int z = 0; z < towerHeight; z++) {
-            		Basic3DBox cBox = new Basic3DBox((x * towerBoxSize) + 80f, (y * towerBoxSize) - 300f, (z * towerBoxSize) + 80f, towerBoxSize, towerBoxSize, towerBoxSize, BoxType.STATIC);
-                    world.addBox(cBox);
-                    boxes.add(cBox);
-                    
-                    ModelInstance instance = new ModelInstance(nonIntersectingModel);
-         			instance.transform.setToTranslation(cBox.getX(), cBox.getY(), cBox.getZ());
-         			boxToModelInstanceMap.put(cBox, instance);
-                }
-            }
-        }
+        // Create some AABBs to process.
+        ArrayList<Basic3DBox> boxes = getRandomBoxes(10, 10, 10, 12345);
         
-        // Create a cluster of dynamic boxes.
-        for(int i = 0; i < clusterSize; i++) {
-        	Basic3DBox cBox = new Basic3DBox((random.nextFloat()*500f) + 80f, 300f, (random.nextFloat()*500f) + 80f, 5, 5, 5, BoxType.DYNAMIC);
-            cBox.applyImpulse(Axis.X, (random.nextFloat()*6f)-3f);
-            cBox.applyImpulse(Axis.Z, (random.nextFloat()*6f)-3f);
-            cBox.setRestitution(random.nextFloat());
-            cBox.setFriction(random.nextFloat());
-            world.addBox(cBox);
-            boxes.add(cBox);
-            
-            ModelInstance instance = new ModelInstance(intersectingModel);
- 			instance.transform.setToTranslation(cBox.getX(), cBox.getY(), cBox.getZ());
- 			boxToModelInstanceMap.put(cBox, instance);
-        }
-        
+ 		for (AABB box : boxes) {	
+ 			ModelInstance instance = new ModelInstance(nonIntersectingModel);
+ 			
+ 			instance.transform.setToTranslation(box.getX(), box.getY(), box.getZ());
+ 			
+ 			instances.add(instance);
+ 		}
+ 
         camController = new CameraInputController(cam);
         Gdx.input.setInputProcessor(camController);
-    }
-
-    @Override
-    public void render () {
-    	camController.update();
-    	
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        
-
-        System.out.println("FPS: " + Gdx.graphics.getFramesPerSecond());
+	}
+	
+	@Override
+	public void render () {
+		camController.update();
 		
-        // Update the physics environment.
-        world.update();
-        
-        
-
-        // Draw all boxes.
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+ 
         modelBatch.begin(cam);
-        for(Basic3DBox box : boxes) {
-        	// Get the model instance.
-        	ModelInstance instance = boxToModelInstanceMap.get(box);
-        	// Draw the instance.
+        for (ModelInstance instance : this.instances) {
         	modelBatch.render(instance, environment);
         }
         modelBatch.end();
-    }
+	}
+	
+	/**
+	 * Create some random AABBs
+	 * @param numberOfBoxes
+	 * @param seed
+	 * @return
+	 */
+	private ArrayList<Basic3DBox> getRandomBoxes(int width, int height, int depth, long seed) {
+        ArrayList<Basic3DBox> boxes = new ArrayList<Basic3DBox>();
+        Random ran = new Random(seed);
+        
+        for (int x = 0; x < width; x++) {
+        	for (int z = 0; z < depth; z++) {
+        		for (int y = 0; y < height; y++) {
+        			// Lets have about a 33% chance of creating a box.
+        			if (ran.nextDouble() <= 0.05) {
+        				boxes.add(new Basic3DBox((x * boxSize), (y * boxSize), (z * boxSize), boxSize, boxSize, boxSize, BoxType.STATIC));
+        			}
+         		}
+     		}
+ 		}
+ 		
+ 		return boxes;
+	}
 }
